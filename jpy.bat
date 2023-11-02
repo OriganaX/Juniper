e9a53e97157cef196398cadb4d0b7e67fea68c6b
Corvin Edler von Schiefner <149676790+RavinClawSecondProfile@users.noreply.github.com>
2023-11-02 11:13:24 +0000

    The Most Up to Date Files
    The Start of This Github will Start with The most updated files

---
+++ jpy.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo Starting in Home Environment
+
+C:\Users\cedle\AppData\Local\Programs\Python\Python312\python.exe %1
---
+++ py.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting in School Environment
+
+C:/ProgramData/App-V/CA9C08A7-BE6C-4E15-A425-C1C9A52819D5/B0ECF97D-8662-42AF-9645-030A99364FAB/root/python.exe %1
